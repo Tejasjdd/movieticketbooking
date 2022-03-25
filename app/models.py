b0bb191f7c5e2d704234434e191b6134410812c2
Tejasjdd <86196048+Tejasjdd@users.noreply.github.com>
2022-03-25 14:27:53 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class Movie(models.Model):
 	name = models.CharField(max_length=25, null=True, default="")
 	duration = models.CharField(max_length=25, null=True, default="")
 	genre = models.CharField(max_length=25, null=True, default="")
-	release_date = models.CharField(max_length=20, null=True, default="")
+	release_date = models.DateTimeField(max_length=20, null=True, default="")
 	language_avail = models.CharField(max_length=50,null=True)
 	shown_in_theater = models.ManyToManyField(Theater)	
 	trailer = models.URLField(blank=True)
